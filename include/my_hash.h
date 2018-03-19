3ad5ebe66169d4c09e0c7a259eddccd95b87fa10
Runquan Ye <yer@eos20.cis.gvsu.edu>
2018-03-18 22:37:35 -0400

    add 3 different hashing method and the T& get method

--- include/my_hash.h
+++ include/my_hash.h
@@ -2,9 +2,10 @@
 #include <cstdlib>
 #include <vector>
 
+#define HASH_SIZE 17011;
 /**
  *This file holds all the hashmap code
- *@author Nabeel Vali
+ *@author Nabeel Vali, Runquan Ye
  */
 
 template <typename T>
@@ -19,7 +20,7 @@ public:
  */
 	
 my_hash(){
-  HASH_SIZE = 17011;
+  //HASH_SIZE = 17011;
   hashData.resize(HASH_SIZE);
 }
 
@@ -42,17 +43,63 @@ bool insert(const T & data, int (*hashingFunc)()){
  *@returns superhero with specified name if present
  */
 T & get(const std::string name){
-
-
-
+	int index = hashData[name];
+	if(hashData[index] -> name == name){
+		return T;
+	}
+	return NULL;
 }
 		
 //we will create the hash functions here 
 
-int hash1(const std::string key){
+int my_hash::Hash1(const std::string key){
+	int hash1 = 0;
+	int index;
+	if(key.length() >= 3){
+		for(int i = key.length() - 1; i > (key.length() - 3); i--){
+			hash1 = hash1 + (int)key[i];
+		}
+	}else{
+		for(int i = key.length(); i > 0; i--){
+			hash1 = hash1 + (int)key[i];
+		}
+	}
+	
+	index = hash1 % HASH_SIZE;
+	
+	return 0;
+}
+
+int my_hash::Hash2(const std::string key){
+	int hash2 = 0;
+	int index;
+
+	for(int i = 0; i< key.length(); i++){
+		hash2 = hash2 + (int)key[i];
+	}
+	
+	index = hash2 % HASH_SIZE;
+
+	return 0;
+}
 
 
-return 0;
+int my_hash::Hash3(const std::string key){
+	int hash3 = 0;
+	int index;
+	if(key.length() >= 3){
+		for(int i = 0; i< 3; i++){
+			hash3 = hash3 + (int)key[i];
+		}
+	}else{
+		for(int i = 0; i< key.length(); i++){
+			hash3 = hash3 + (int)key[i];
+		}
+	}
+	
+	index = hash3 % HASH_SIZE;
+
+	return 0;
 }
 
 private:
@@ -61,5 +108,5 @@ private:
   std::vector<std::vector<T> > hashData;
 
   /**Size of the hashtable .**/
-  const int HASH_SIZE;
+  static const int HASH_SIZE;
 };
