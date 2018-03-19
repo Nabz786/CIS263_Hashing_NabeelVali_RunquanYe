d44260beb6aa937a7064a0b21ef5c7b0f3766c6c
Runquan Ye <yer@eos20.cis.gvsu.edu>
2018-03-18 22:45:30 -0400

    add the code comments and fix the form.

--- include/superhero.h
+++ include/superhero.h
@@ -4,60 +4,67 @@
 
 /** 
  *This class represents an interface for our superhero class
- *@author Nabeel Vali
+ *@author Nabeel Runquan Ye
  */
 
 class superhero{
 
 public:
+	/**
+	 *Constructor to initialize the super hero
+	 */	
+  	superhero(int page_id, std::string name, std::string urlslug, std::string id,
+		std::string alignment, char eye_color, char hair_color, char sex,
+	  	std::string gsm, bool alive, int app,
+            	std::string first_app, int year);
 
-  superhero(int page_id, const std::string &name, const std::string &urlslug, const std::string &id,
-	const std::string &alignment, char eye_color, char hair_color, char sex,
-	  const std::string &gsm, bool alive, int appearances,
-           const std::string &first_appearance, int year);
+	/**
+	 *get super hero's name
+	 *@return string, super hero's name
+	 */
+	std::string getName(); 
 
-std::string getName() const;
 
 
 private:
+	/**Vector of vectors to hold our data*/
+  	int page_id;
 
-  int page_id;
+	/**super hero's name*/
+  	std::string name;
 
+	/**super hero's url slug*/
+  	std::string urlslug;
 
-  std::string name;
+	/**super hero's id**/
+  	std::string id;
 
+	/**super hero's alignment*/
+  	std::string alignment;
 
-  std::string urlslug;
+	/**super hero's eye color*/
+  	char eye_color;
+	
+	/**super hero's hair color*/
+  	char hair_color;
 
+	/**super hero's gender*/
+  	char sex;
 
-  std::string id;
+	/**super hero's gsm*/
+  	std::string gsm;
 
+	/**is the superhero still alive*/
+  	bool alive;
 
-  std::string alignment;
+	/**super hero's appearance time*/
+  	int appearances;
 
+	/**super hero's first appearance*/
+  	std::string first_appearance;
 
-  char eye_color;
-
-
-  char hair_color;
-
-
-  char sex;
-
-
-  std::string gsm;
-
-
-  bool alive;
-
-
-  int appearances;
-
-
-  std::string first_appearance;
-
-
-  int year;	
+	/**super hero's publish year*/
+  	int year;	
 };
 
 #endif
