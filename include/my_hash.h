633dcce760622f9cef7c0086ebf2bc1ca6037db5
Runquan Ye <yer@eos20.cis.gvsu.edu>
2018-03-18 22:42:22 -0400

    add the comment and edit the form of the code

--- include/my_hash.h
+++ include/my_hash.h
@@ -13,100 +13,112 @@ template <typename T>
 class my_hash{
 
 
-public:
+  public:
 
-/**
- *Constructor to initialize our hashtable
- */
+	/**
+	 *Constructor to initialize our hashtable
+	 */
 	
-my_hash(){
-  //HASH_SIZE = 17011;
-  hashData.resize(HASH_SIZE);
-}
-
-/**
- *Returns true or false depending on whether there was a collision
- *upon inserting the data
- *@returns true or false if collision occurred
- */		
-bool insert(const T & data, int (*hashingFunc)()){
-	int index = (*hashingFunc(data.getName()));
-	hashData[index].insert(hashData[index].begin, std::swap(data));
-	if(hashData[index].size > 1){
-		return true;
+	my_hash(){
+	  //HASH_SIZE = 17011;
+	  hashData.resize(HASH_SIZE);
 	}
-	return false;
-}
 
-/**
- *Returns data based on the name parameter
- *@returns superhero with specified name if present
- */
-T & get(const std::string name){
-	int index = hashData[name];
-	if(hashData[index] -> name == name){
-		return T;
-	}
-	return NULL;
-}
-		
-//we will create the hash functions here 
-
-int my_hash::Hash1(const std::string key){
-	int hash1 = 0;
-	int index;
-	if(key.length() >= 3){
-		for(int i = key.length() - 1; i > (key.length() - 3); i--){
-			hash1 = hash1 + (int)key[i];
+	/**
+	 *Returns true or false depending on whether there was a collision
+	 *upon inserting the data
+	 *@returns true or false if collision occurred
+	 */		
+	bool insert(const T & data, int (*hashingFunc)()){
+		int index = (*hashingFunc(data.getName()));
+		hashData[index].insert(hashData[index].begin, std::swap(data));
+		if(hashData[index].size > 1){
+			return true;
 		}
-	}else{
-		for(int i = key.length(); i > 0; i--){
-			hash1 = hash1 + (int)key[i];
+		return false;
+	}
+
+	/**
+	 *Returns data based on the name parameter
+	 *@returns superhero with specified name if present
+	 */
+	T & get(const std::string name){
+		int index = hashData[name];
+		if(hashData[index] -> name == name){
+			return T;
 		}
+		return NULL;
 	}
 	
-	index = hash1 % HASH_SIZE;
 	
-	return 0;
-}
+	//we will create the hash functions here 
 
-int my_hash::Hash2(const std::string key){
-	int hash2 = 0;
-	int index;
-
-	for(int i = 0; i< key.length(); i++){
-		hash2 = hash2 + (int)key[i];
-	}
-	
-	index = hash2 % HASH_SIZE;
-
-	return 0;
-}
-
-
-int my_hash::Hash3(const std::string key){
-	int hash3 = 0;
-	int index;
-	if(key.length() >= 3){
-		for(int i = 0; i< 3; i++){
-			hash3 = hash3 + (int)key[i];
+	/**
+	 *First hashing method is bash on the last 3 characters of the key.
+	 *@returns 0 if the method finish
+	 */
+	int my_hash::Hash1(const std::string key){
+		int hash1 = 0;
+		int index;
+		if(key.length() >= 3){
+			for(int i = key.length() - 1; i > (key.length() - 3); i--){
+				hash1 = hash1 + (int)key[i];
+			}
+		}else{
+			for(int i = key.length(); i > 0; i--){
+				hash1 = hash1 + (int)key[i];
+			}
 		}
-	}else{
+	
+		index = hash1 % HASH_SIZE;
+	
+		return 0;
+	}
+
+	/**
+	 *second hashing method is bash on all the characters of the key.
+	 *@returns 0 if the method finish
+	 */
+	int my_hash::Hash2(const std::string key){
+		int hash2 = 0;
+		int index;
+
 		for(int i = 0; i< key.length(); i++){
-			hash3 = hash3 + (int)key[i];
+			hash2 = hash2 + (int)key[i];
 		}
-	}
 	
-	index = hash3 % HASH_SIZE;
+		index = hash2 % HASH_SIZE;
 
-	return 0;
-}
+		return 0;
+	}
 
-private:
+	/**
+	 *Last hashing method is bash on the first 3 characters of the key.
+	 *@returns 0 if the method finish
+	 */
+	int my_hash::Hash3(const std::string key){
+		int hash3 = 0;
+		int index;
+		if(key.length() >= 3){
+			for(int i = 0; i< 3; i++){
+				hash3 = hash3 + (int)key[i];
+			}
+		}else{
+			for(int i = 0; i< key.length(); i++){
+				hash3 = hash3 + (int)key[i];
+			}
+		}
+	
+		index = hash3 % HASH_SIZE;
+
+		return 0;
+ 	}
+
+  private:
   
-  /**Vector of vectors to hold our data .**/
-  std::vector<std::vector<T> > hashData;
+	  /**Vector of vectors to hold our data .**/
+	  std::vector<std::vector<T> > hashData;
 
-  /**Size of the hashtable .**/
-  static const int HASH_SIZE;
+	  /**Size of the hashtable .**/
+	  static const int HASH_SIZE;
 };
