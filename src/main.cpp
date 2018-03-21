d2c4fef842e2a49a2e9f8988f8a92463deb57333
Nabeel Vali <valin@eos25.cis.gvsu.edu>
2018-03-20 22:12:31 -0400

    Finalized main, added comments, tested getName()

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,42 @@
 #include "my_hash.h"
 #include "superhero.h"
 
+/**
+ *Main class of hashing program, here all functions and the 
+ *two public methods can be tested
+ *
+ *Important to specify the superhero to be searched
+ *please modify the variable nameSearch with the exact name
+ *All insertion functions are ran and require no further input
+ *The number of collisions will be output to the console 
+ *
+ * @author Nabeel Vali
+ * @Version 1.0 Winter 2018
+ **/
 
 
 int main(int argc, char** argv){
-
-	char getEyeColor(std::string &eyeColor);
-	char getHairColor(std::string &hairColor);
+	
+	
 	char getSex(std::string &sex);
 	bool isAlive(std::string &alive);
 
+	/**Instances of my_hash used to test each indiviual hashing function **/
+	my_hash<superhero> hash1;
+	my_hash<superhero> hash2;
+	my_hash<superhero> hash3;
 
-	 my_hash<superhero> hash1;
-	 int collisions = 0;
+	/**Collion counters for the three hash functions **/
+	int collision1 = 0;
+	int collision2 = 0;
+	int collision3 = 0;
 
+	/**This function can be changed according to the name of the superhero to be searched for **/
+	const std::string nameSearch = "Emma Frost (Earth-616)";
 
-	 io::CSVReader<13, io::trim_chars<' '>, io::double_quote_escape<',','\"'> > in("superherodata.csv");
- 	 in.read_header(io::ignore_extra_column,
+	//Implementation of CSV parser mentioned in class and found on github
+	io::CSVReader<13, io::trim_chars<' '>, io::double_quote_escape<',','\"'> > in("superherodata.csv");
+ 	in.read_header(io::ignore_extra_column,
 			 "page_id",
 			 "name",
 			 "urlslug",
@@ -34,49 +54,58 @@ int main(int argc, char** argv){
 			 "FIRST APPEARANCE",
 			 "Year");
 
- 	 int page_id;
-	 std::string name;     
-	 std::string urlslug;
-	 std::string id;
-	 std::string align;
-	 std::string eye;	
-	 std::string hair;	
-	 std::string sex;	
-	 std::string gsm;
-	 std::string alive;	
-	 int appearances;
-	 std::string first_appearance;
-	 int year;
+	//Variables representing each column name in the csv file
+ 	int page_id;
+	std::string name;     
+	std::string urlslug;
+	std::string id;
+	std::string align;
+	std::string eye;	
+	std::string hair;	
+	std::string sex;	
+	std::string gsm;
+	std::string alive;	
+	int appearances;
+	std::string first_appearance;
+	int year;
 
-	 while(in.read_row(page_id, name, urlslug, id, align, eye, hair, sex, gsm, alive, appearances, first_appearance, year)){
-		superhero newHero(page_id, name, urlslug, id, align, eye, hair, getSex(sex), gsm, isAlive(alive), appearances, first_appearance, year);
-		if(hash1.insert(newHero))
-			collisions++;	 		
+	//While full columns of data are present we will create new superhero objects
+	//passing them to the three hashing functions
+	while(in.read_row(page_id, name, urlslug, id, align, eye
+		, hair, sex, gsm, alive, appearances, first_appearance, year)){
+		
+		superhero newHero(page_id, name, urlslug, id, align, eye, hair, getSex(sex),
+					 gsm, isAlive(alive), appearances, first_appearance, year);
+	
+		//Upon insertion if we obtain a collison we will increment the collions counter
+		if(hash1.insert(newHero, 1))
+			collision1++;
+		if(hash2.insert(newHero, 2))
+			collision2++;
+		if(hash3.insert(newHero, 3))
+			collision3++;	 		
          }
 
-	std::cout << "Collisions: " << collisions << std::endl;
+
+
+	//All results of searches and hashes will be outputed here
+	
+	try{
+		hash2.get(nameSearch);
+	}catch(const char * str){
+		std::cout << str << std::endl;
+	}
+
+	std::cout << "Collisions for hash 1: " << collision1 << std::endl;
+	std::cout << "Collisions for hash 2: " << collision2 << std::endl;
+	std::cout << "Collisions for hash 3: " << collision3 << std::endl;
 }
 
-
-
-
-
-//	char getEyeColor(std::string &eyeColor){
-//		if(!eyeColor.length() == 0){
-//			return eyeColor.at(0);
-//		}else{
-//			return 'U';
-//		}
-//	}
-
-//	char getHairColor(std::string &hairColor){
-//		if(!hairColor.length() == 0){
-//			return hairColor.at(0);
-//		}else{
-//			return 'U';
-//		}
-//	}
-
+	/**
+	*Returns the first character in the string of the superhero's
+	*Sex identifier 
+	*@returns Char - representation of superhero's sex
+	*/ 	
 	char getSex(std::string &sex){
 		if(!sex.length() == 0){
 			return sex.at(0);
@@ -84,6 +113,11 @@ int main(int argc, char** argv){
 			return 'U';
 		}
 	}
+
+	/**
+	*Returns a boolean value representing the living status of a superhero
+	*@returns boolean - if the superhero is alive or not
+	**/
 
 	bool isAlive(std::string &alive){
 		if(!alive.empty() && alive.at(0) == 'L'){
